567976bc762607fa34b26ceb82ab9ede259a5fca
June McEnroe <june@causal.agency>
2022-09-18 21:57:06 -0400

    Make copyrights and AUTHORS consistent

--- xdg.c
+++ xdg.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 2019, 2020  C. McEnroe <june@causal.agency>
+/* Copyright (C) 2019, 2020  June McEnroe <june@causal.agency>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
